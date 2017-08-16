180d499da95b3980c5535cb13f7bea4dcfd799f3
vaboston <valentin.bouin@me.com>
2017-08-16 20:21:41 +0200

    turn around

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var mysql = require('mysql');
+var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser')
 var app = express();
 var sys = require('sys')
@@ -11,7 +12,49 @@ app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({   
 	extended: true
 })); 
-	
+
+
+
+var cookieParser = require('cookie-parser');
+app.use(cookieParser({secret: 'toto'}))
+app.use(cookieSession({
+  name: 'session',
+  keys: ['TOTOTO'],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+
+
+// app.use(cookieParser(config.cookieSecret))
+// // need cookieParser middleware before we can do anything with cookies
+// app.use(express.cookieParser());
+
+// // set a cookie
+// app.use(function (req, res, next) {
+//   // check if client sent cookie
+//   var cookie = req.cookies.cookieName;
+//   if (cookie === undefined)
+//   {
+//     // no: set a new cookie
+//     var randomNumber=Math.random().toString();
+//     randomNumber=randomNumber.substring(2,randomNumber.length);
+//     res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
+//     console.log('cookie created successfully');
+//   } 
+//   else
+//   {
+//     // yes, cookie was already present 
+//     console.log('cookie exists', cookie);
+//   } 
+//   next(); // <-- important!
+// });
+
+
+
+
+// req.signedCookies['name']
+
 
 var connection = mysql.createConnection({
 	host: 'localhost',
@@ -20,8 +63,21 @@ var connection = mysql.createConnection({
 	database: 'appliFood' 
 });
 
+var JeanClaude = 'MaRecette';
 
 app.get("/",function(req,res){
+
+
+    // read cookies
+    console.log(req.cookies) 
+
+    
+
+
+
+	tototo = JeanClaude;
+
+	console.log(req.body);
 	connection.connect(function(err, callback){
 		listeIngredient = [];
 		connection.query('SELECT Nom FROM Ingredient', function(err, rows, fields) {
@@ -44,7 +100,10 @@ app.get("/",function(req,res){
 			//console.log("liste : ", liste);
 		});
 		connection.query('SELECT * FROM Recette', function(err, rows, fields) {
-			res.render('home', {results: rows});
+			// if (req.session.views) {console.log(res.session.views);}
+			console.log(req.session.views)
+			Gras = req.session.views
+			res.render('home', {results: rows, gras: 'Gras', select: 'selected'});
 			if (!err)
 				//console.log('The solution is: ', rows);
 				console.log('The solution is:');
@@ -54,6 +113,19 @@ app.get("/",function(req,res){
 
 	});
 });
+
+app.post("/", function (req, res) {
+console.log("body /", req.body)
+queryString = 'SELECT * FROM Recette WHERE Categorie = ' + connection.escape(req.body.Categorie) + ' and Gras = ' + connection.escape(req.body.Gras) + ' order by rand() limit 1'
+	connection.query(queryString, function(err, rows, fields) {
+		res.render('home', function(err, html){
+			var JeanClaude = rows;
+			console.log("on set jc")
+			console.log(JeanClaude)
+			tototo = rows;
+		});
+	})
+})
 
 app.post('/addRecette', function (req, res) {
 	console.log("body", req.body)
@@ -141,5 +213,36 @@ app.post('/delRecette', function (req, res) {
 
 					});
 			});	
+});
+
+app.post('/getRecette', function (req, res) {
+	console.log(req.body)
+	console.log('Get recette call')
+	number = req.body.recette
+
+// select * from table order by rand() limit 10
+
+
+   queryString = 'SELECT * FROM Recette WHERE Categorie = ' + connection.escape(req.body.Categorie) + ' and Gras = ' + connection.escape(req.body.Gras) + ' order by rand() limit 1'
+	connection.query(queryString, function(err, rows, fields) {
+		let options = {
+	        maxAge: 1000 * 60 * 15, // would expire after 15 minutes
+	        httpOnly: true, // The cookie only accessible by the web server
+	        signed: true // Indicates if the cookie should be signed
+	    }
+
+	    // Set cookie
+    	req.session.views = 'Gras'
+    	console.log('gras :  ?', req.session.views);
+		res.render('home', {results: rows, gras: req.session.views, select: 'selected'});
+
+			// res.render('home', {results: rows});
+			if (!err)
+				//console.log('The solution is: ', rows);
+				console.log('The solution is:');
+			else
+				console.log('Error while performing Query.');
+		});
+
 });
 app.listen(3000);
